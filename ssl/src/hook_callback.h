f714b38c67e896acee55a34c80533e3b83b20cdc
cc <wangcheng@tingyun.com>
2016-11-29 20:42:22 -0800

    delete src

--- ssl/src/hook_callback.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-namespace hook_callback {
-    class logic_handler {
-    public:
-        virtual void on_task(const char *) = 0;
-    };
-}
--- ssl/util/vstl/L3/io/aio.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#ifdef _WIN32
-#include "iocp_aio_th.h"
-#else
-#include "linux_aio_th.h"
-#endif
